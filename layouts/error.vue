85ed497622d1c847d013e6a25f895e085204ff55
willfai <will@fai2.com>
2022-02-01 08:29:30 -0500

    blank error.vue for production

---
+++ layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<!-- {{error}} -->
+		<Nuxt />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'BlankLayout',
+	// props: ['error']
+	// data() {
+	// 	return {}
+	// },
+}
+</script>
+
+
+<style lang="scss">
+	html {
+		background: transparent;
+		overflow: auto;
+	}
+</style>
